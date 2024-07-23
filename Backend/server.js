b85101b8255209727dab83a3e355404532edad68
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-07-24 00:31:08 +0530

    few changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const connectDB = require("./config/db1");
 const app = express();
 app.use(cors());
 dotenv.config();
 const port = process.env.PORT || 3000;
 const { chats } = require("./data/data");
+connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
