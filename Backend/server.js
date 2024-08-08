3dc1a268add17c79bbd1d0c0dba2df690cd775b9
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-08-08 21:46:35 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,18 +2,26 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db1");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(cors());
 dotenv.config();
 const port = process.env.PORT || 3000;
 const { chats } = require("./data/data");
 connectDB();
+
+app.use(express.json()); //to accept JSON data
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/user", userRoutes);
+
 app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
+
 app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
